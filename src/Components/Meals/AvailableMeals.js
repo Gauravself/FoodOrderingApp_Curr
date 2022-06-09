c3ba9c92efb5fc4dec35f0794a6c933b11d79891
Gaurav Rastogi <rastogi.g1998@gmail.com>
2022-06-09 10:22:48 +0530

    Setting Meal Display

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -2,9 +2,18 @@ import React from "react";
 import classes from "./AvailableMeals.module.css";
 import DUMMY_MEALS from "./dummyMealsData";
 import Card from "../UI/Card";
+import MealItem from "./MealItems/MealItem";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   return (
     <section className={classes.meal}>
       <Card>
