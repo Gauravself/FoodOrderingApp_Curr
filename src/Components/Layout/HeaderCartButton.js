ef5bb892beb684d9d05e98229f34b0ad9eb5cfe0
Gaurav Rastogi <rastogi.g1998@gmail.com>
2022-06-20 16:32:32 +0530

    Adding bump effect to Cart

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,15 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnHighlighted, setbtnHighlighted] = useState(false);
   const cartCTX = useContext(CartContext);
   const numOfCartItems = cartCTX.item.reduce((acc, curr) => {
     return acc + curr.amount;
   }, 0);
+
+  const { item } = cartCTX;
+
+  const buttonClasses = `${classes.button} ${
+    btnHighlighted ? classes.bump : ""
+  }`;
+  useEffect(() => {
+    if (item.length === 0) return;
+    setbtnHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setbtnHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [item]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
